22f216af75ed301f8b28820d18769f61b39eb0d2
Nick Miyake <nmiyake@palantir.com>
2018-03-12 22:07:34 -0700

    Update go-ptimports dependency to 2.1.0

--- main.go
+++ main.go
@@ -12,20 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
@@ -41,7 +27,7 @@ func main() {
 	rootCmd := assetapi.RootCommand(
 		assetName,
 		func(cmd *cobra.Command, args []string) error {
-			return assetapi.RunAmalgomatedFormatCommand(assetName, args, assetapi.ListFlagVal, nil, cmd.OutOrStdout(), cmd.OutOrStderr())
+			return assetapi.RunAmalgomatedFormatCommand(assetName, args, assetapi.ListFlagVal, []string{"--refactor"}, cmd.OutOrStdout(), cmd.OutOrStderr())
 		},
 	)
 	assetapi.AmalgomatedMain(assetName, rootCmd, amalgomatedformatter.Instance())
